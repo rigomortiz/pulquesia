7d9422a8fba0fb247c57599931d91fe1bce47bad
rigomortiz <rigobertocanseco@comunidad.unammx>
2025-09-22 13:24:29 -0600

    Add loteria post

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
-      <p className="text-2xl text-center dark:text-white">
-        <Link href="/">{name}</Link>
-      </p>
+        <img className="size-48" alt="" src="/images/logo.png" />
     </header>
   );
 }
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -7,7 +7,7 @@ import remarkGfm from 'remark-gfm';
 import rehypeUnwrapImages from 'rehype-unwrap-images';
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'posts');
+export const POSTS_PATH = path.join(process.cwd(), 'eventos');
 
 // getPostFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const getPostFilePaths = () => {
